8d05c91e9da0c5958127e87992e7af710706e8c8
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-09-15 23:06:55 -0230

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,15 +4,16 @@ import Welcome from './screens/Welcome';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Dashboard from './screens/Dashboard';
+import Meditate from './screens/Meditate';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
     <Stack.Screen name="Home" component={Welcome}   options={{headerShown: false}} />
+        <Stack.Screen name="meditate" component={Meditate}    options={{headerShown: false}}/>
     <Stack.Screen name="Dashboard" component={Dashboard}    options={{headerShown: false}}/>
     </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
